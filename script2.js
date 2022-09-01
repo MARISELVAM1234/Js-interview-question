e2a90125659416a610e76236e11df736f58d1dfd
selvam <selvamplkm111@gmail.com>
2022-09-01 22:54:19 +0530

    add-code.

---
+++ script2.js
@@ -0,0 +1,33 @@
+// let input = "selvamplkm111@gmail.com"
+// let a = input.split("@")
+// let b = a[0]
+// let c = "aeiou"
+
+// console.log(b);
+// for(i = 0 ; i<b.length ;i++){
+//     for(j=0 ;j<c.length ; j++){
+//         if (b[i] == c[j]){
+//             // console.log(b[i]);
+//             break;
+//         }
+//         else{
+//             console.log(b[i]);
+//         }
+
+//     }
+// }
+
+let a = ["one","two","three","four","five"];
+let emp =[]
+let sum = 0
+let vow = ["a" ,"e" ,"i" ,"o" ,"u"]
+for (let i = 0; i < a.length; i++) {
+    let element = a[i];
+    console.log(element);
+    for(let j= 0 ; j < element.length; j++){
+        console.log(element[j]);
+        
+
+    }
+    
+}
